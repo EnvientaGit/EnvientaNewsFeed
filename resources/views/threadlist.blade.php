458dd857c82a41b9e80240440b46e22b66267e31
fly <fly@bitbox.hu>
2018-12-19 18:13:09 +0100

    cannot find Thread class - help! :)

--- resources/views/threadlist.blade.php
+++ resources/views/threadlist.blade.php
@@ -2,7 +2,6 @@
 <a class="nav-link" id="v-pills-mentoring-tab" data-toggle="pill" href="#v-pills-mentoring" role="tab" aria-controls="v-pills-mentoring" aria-selected="false">Mentoring</a>
 <a class="nav-link" id="v-pills-investment-tab" data-toggle="pill" href="#v-pills-investment" role="tab" aria-controls="v-pills-investment" aria-selected="false">Investment informations</a>
  
-
 @foreach ($threads as $t)
 	<a class="nav-link active" id="v-pills-home-tab" data-toggle="pill" href="#v-pills-home" role="tab" aria-controls="v-pills-home" aria-selected="true">Valami</a>
 @endforeach
